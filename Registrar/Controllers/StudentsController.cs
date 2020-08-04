8f1e552d7526159a8c0d7e7845285151bf741681
Sara Kane <kane.sarak@gmail.com>
2020-08-04 11:01:53 -0700

    Add functionality to view all students and add a new student
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

---
+++ Registrar/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Registrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Registrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly RegistrarContext _db;
+
+    public StudentsController (RegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Students.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
